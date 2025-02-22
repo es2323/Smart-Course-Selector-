5c70bedd040871462f8dfb0ee421a023951cff8d
EdwardOpenSource <edwardbhodhojnr@gmail.com>
2025-02-22 18:56:52 +0000

    Added existing Django project

---
+++ Smart_Course_Selector/quiz/models.py
@@ -0,0 +1,18 @@
+from django.db import models
+
+class Question(models.Model):
+    text = models.CharField(max_length=255)
+
+class Answer(models.Model):
+    question = models.ForeignKey(Question, on_delete=models.CASCADE)
+    text = models.CharField(max_length=255)
+    score = models.IntegerField()
+
+class QuizSession(models.Model):
+    current_question = models.ForeignKey(Question, null=True, on_delete=models.SET_NULL)
+    answers = models.ManyToManyField(Answer)
+
+class Recommendation(models.Model):
+    degree_name = models.CharField(max_length=255)
+    criteria = models.TextField()
+
