d6c5a697268fddad1b57543bb3c2045db6bb5ed3
MurtazaAliCS <Murtaza.Ali11@outlook.com>
2025-02-25 20:34:42 +0000

    Updated Answer model in models.py

--- Smart_Course_Selector/quiz/models.py
+++ Smart_Course_Selector/quiz/models.py
@@ -9,6 +9,8 @@ class Answer(models.Model):
     question = models.ForeignKey(Question, on_delete=models.CASCADE)
     text = models.CharField(max_length=255)
     score = models.IntegerField()
+    def __str__(self):
+        return self.text
 
 class QuizSession(models.Model):
     current_question = models.ForeignKey(Question, null=True, on_delete=models.SET_NULL)
